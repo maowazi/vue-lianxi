961a4a2f443ae4275ad387692a62d40e7013e146
shengchangjian <1121090437@qq.com>
2020-11-18 11:29:12 +0800

    修改

--- 组件应用复杂版自写/src/App.vue
+++ 组件应用复杂版自写/src/App.vue
@@ -1,10 +1,10 @@
 <template>
-  <div id="app">
-    <h1>简单组件练习</h1>
-    <Add @handadd="handadd"/>
-    <List :xinxi="xinxi" :dataval="dataval"/>
-    <Todo @diban="diban" @wancheng="wancheng"/>
-  </div>
+    <div id="app">
+        <h1>简单组件练习</h1>
+        <Add @handadd="handadd" />
+        <List :xinxi="xinxi" :dataval="dataval" />
+        <Todo @diban="diban" @wancheng="wancheng" />
+    </div>
 </template>
 
 <script>
@@ -12,34 +12,34 @@ import Add from "./components/add";
 import List from "./components/list";
 import Todo from "./components/todo";
 export default {
-  name: 'app',
-  data() {
-      return {
-          xinxi:"daiban",
-          dataval:[]
-      }
-  },
-  components: {
-      Add,
-      List,
-      Todo
-  },
-  methods: {
-      diban:function(val){
-          this.xinxi = val;
-      },
-      wancheng:function(val){
-          this.xinxi = val;
-      },
-      handadd:function(val){
-          this.dataval = val
-      }
-  },
-}
+    name: "app",
+    data() {
+        return {
+            xinxi: "daiban",
+            dataval: []
+        };
+    },
+    components: {
+        Add,
+        List,
+        Todo
+    },
+    methods: {
+        diban: function(val) {
+            this.xinxi = val;
+        },
+        wancheng: function(val) {
+            this.xinxi = val;
+        },
+        handadd: function(val) {
+            this.dataval = val;
+        }
+    }
+};
 </script>
 
 <style>
-*{
+* {
     margin: 0;
     padding: 0;
 }
