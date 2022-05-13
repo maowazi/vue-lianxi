834dd5b32c5e7a9bbc463ff94dfe01d1390f8741
v_cqsheng(盛存钱) <v_cqsheng@tencent.com>
2022-05-13 10:57:54 +0800

    v-model使用

---
+++ v-model使用/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <custom-input v-model="searchText"></custom-input>
+    <p>{{searchText}}</p>
+  </div>
+</template>
+
+<script>
+import customInput from './components/HelloWorld.vue';
+export default {
+  name: "App",
+  components:{
+    customInput
+  },
+  data() {
+    return {
+      searchText:''
+    };
+  },
+  methods: {
+    
+  },
+};
+</script>
+
+<style>
+#app {
+}
+.active {
+  background: red;
+}
+</style>
---
+++ v-model使用/src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="hello">
+    <h1>msg</h1>
+    <input
+      @input="$emit('input', $event.target.value)"
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
